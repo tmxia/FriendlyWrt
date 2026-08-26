b62aeb71a01f06ab10125843bb13549c9ddd4009
tmxia <tmxia1000@gmail.com>
2026-08-26 12:28:54 +0800

    更新 add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -21,7 +21,7 @@ EOF
 
 # Packages to ensure
 ENSURE_PKGS="
-bc vsftpd sudo unzip file procd logrotate coreutils-stat lsof jq wireguard-tools python3-light
+bc vsftpd sudo unzip file logrotate coreutils-stat lsof jq wireguard-tools python3-light
 netdata luci-app-cpufreq luci-app-netdata
 "
 
