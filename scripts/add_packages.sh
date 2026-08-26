da1d290a8e30f4c4b118471adef3575939ea0062
tmxia <tmxia1000@gmail.com>
2026-08-26 20:34:05 +0800

    更新 add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -115,8 +115,6 @@ done
 # Non-interactive resolve dependencies, preserving manually set kernel options
 make olddefconfig
 
-make olddefconfig
-
 # Brief kernel config check
 grep -q "^CONFIG_INET_DIAG=y" .config && echo "KERNEL: CONFIG_INET_DIAG=y" || echo "KERNEL: CONFIG_INET_DIAG not set"
 
